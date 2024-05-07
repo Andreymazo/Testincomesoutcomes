d2fcc7acc299c154e8d6d171a9f66993ffc76dbf
andrey_mazo <andreymazo@mail.ru>
2024-05-08 02:43:55 +0300

    first commit

--- add_line.py
+++
@@ -1,6 +0,0 @@
-def add_line(date, category, amount, comment ):
-    with open ('our_file.txt', 'a') as f:
-        f.write(f'\n{date}, {category}, {amount}, {comment}')
-
-if __name__ == '__main__':
-    add_line('g41', '45', 'rent5', 'Расход' )
